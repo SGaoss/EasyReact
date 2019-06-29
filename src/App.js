afe741d88838b55764b499e84f80357573097932
SGaoss <1142506170@qq.com>
2019-06-29 16:45:41 +0800

    简单尝试渲染页面

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Fragment} from 'react';
 
+let Person = {
+  height: 168,
+  weight:110,
+  cost:16,
+  type:'哈皮'
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Fragment>
+     <div className="box">
+       <h1>此恋之洁，无以复加</h1>
+     </div>
+    <div className="person">
+      <h2>{Person.cost}</h2>
     </div>
-  );
+    </Fragment>
+  )
 }
 
 export default App;
